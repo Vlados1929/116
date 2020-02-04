b9ca187d9005a3f54abff487ca65180a5395a6c4
misha1010 <60483223+misha1010@users.noreply.github.com>
2020-02-04 19:43:17 +0200

    Update Mary test.java
    Add name for document

--- Mary test.java
+++ Mary test.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main() {
 
-        String s = "Тестовый документ";
+        String s = "Тестовый документ QA Testers";
         System.out.println(s);
     }
 }
