eed8495bd6cbb7e69ab32afe5cb1ef4864b7db43
AlexanderRudnichenko <izloystrelok@gmail.com>
2020-02-05 00:46:15 +0300

    Some edits #3

--- Hello.java
+++ Hello.java
@@ -2,7 +2,15 @@
 * Hello
 */
 public class Hey there {
+    //Some more edits
+	//Have a nice day. ^^
+}
+
+/*
+public class Hey there {
     public static void main(String[] args) {
         system.out.println("Hello my young Padawans!");
     }
+	//Have a nice day. ^^
 }
+*/
